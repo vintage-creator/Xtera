c53e3a0dcb56115a8a292836258e8dc423107653
Israel C. Abazie <chuksy3@gmail.com>
2025-05-01 21:55:45 +0100

    fix: serve favicon and static assets correctly from Express server

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ require("dotenv").config();
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const path = require("path");
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/favicon.svg", (req, res) => {
+  res.sendFile(path.join(__dirname, "favicon.svg"));
+});
+
+// Serve index.html for the root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // Endpoint to fetch crypto prices using CoinGecko API
 app.get('/api/tickers', async (req, res) => {
